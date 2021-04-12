89826b0ff81116928e5b02eed2bc1d58e8dc468a
Leighton <63763567+leighton-0@users.noreply.github.com>
2021-04-12 16:40:08 +0800

    Update kali-setup-script.sh

--- kali-setup-script.sh
+++ kali-setup-script.sh
@@ -53,8 +53,8 @@ rm -r .bash_aliases
 #mkdir -r .bash_aliases
 #cd .bash_aliases
 #curl -k -s https://raw.githubusercontent.com/leighton-0/kali-setup/master/.bash_aliases
-#wget -P '.bash_aliases' https://raw.githubusercontent.com/leighton-0/kali-setup/master/.bash_aliases
-wget -P https://raw.githubusercontent.com/leighton-0/kali-setup/master/.bash_aliases
+wget -P '.bash_aliases' https://raw.githubusercontent.com/leighton-0/kali-setup/master/.bash_aliases
+#wget -P https://raw.githubusercontent.com/leighton-0/kali-setup/master/.bash_aliases
 #cd ..
 
 

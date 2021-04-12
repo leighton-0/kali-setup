8fe8ced72c4fa771772e34d27aa455e1735e79da
Leighton <63763567+leighton-0@users.noreply.github.com>
2021-04-13 00:04:15 +0800

    Update kali-setup-script.sh

--- kali-setup-script.sh
+++ kali-setup-script.sh
@@ -60,11 +60,11 @@ wget https://raw.githubusercontent.com/leighton-0/kali-setup/master/.bash_aliase
 
 
 printf '\n============================================================\n'
-    printf '[+] Auto MAC spoof on start up - assuming wlan1'
+    printf '[+] Auto MAC spoof on start up - assuming wlan0'
     printf '============================================================\n\n'
  touch /etc/systemd/system/changemac@.service
 wget -P /etc/systemd/system https://raw.githubusercontent.com/leighton-0/kali-setup/master/changemac@.service
-systemctl enable changemac@wlan1.service
+systemctl enable changemac@wlan0.service
 sleep $s
 
    printf '\n============================================================\n'

b72e0dbf0d9ac3a247792f8ee140a5762883b6a7
Leighton <63763567+leighton-0@users.noreply.github.com>
2022-01-15 03:13:14 +0000

    Update kali-setup-script_1.sh

--- kali-setup-script_1.sh
+++ kali-setup-script_1.sh
@@ -7,12 +7,16 @@
 #    installation
 # clone the repo
 $ git clone https://github.com/sherlock-project/sherlock.git
+ 
 
 # change the working directory to sherlock
 $ cd sherlock
-
 # install the requirements
 $ python3 -m pip install -r requirements.txt
+
+ 3 clipit
+  apt install clipit 3 clipit
+  apt install clipit
 
 multi-line comment
 
